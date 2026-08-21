b0c604c2876f12c4abab345918d225d9ec863dcc
juanperez238421-cpu <juanperez238421@gmail.com>
2026-08-21 09:32:57 -0500

    Add OOP Logic Lab configuration

---
+++ t3/oop-logic-01/config.js
@@ -0,0 +1,24 @@
+window.IJR_OOP_LOGIC_CONFIG = Object.freeze({
+  activitySlug: 'seminar11-oop-logic-01-2026',
+  supabaseUrl: 'https://rlfxnjbqxbozjdzkbwlz.supabase.co',
+  supabasePublishableKey: 'sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87',
+  sessionKey: 'ijr-seminar11-oop-logic01-active-v1',
+  teacherSessionKey: 'ijr-seminar11-oop-logic01-teacher-v1',
+  targetMinutes: 40,
+  questionCount: 12,
+  variantBankSize: 36,
+  helpTokenLimit: 3,
+  rpc: {
+    start: 'seminar_oop_start_team_v1',
+    resume: 'seminar_oop_resume_v1',
+    submit: 'seminar_oop_submit_v1',
+    help: 'seminar_oop_use_help_v1',
+    reveal: 'seminar_oop_reveal_v1',
+    skip: 'seminar_oop_skip_v1',
+    event: 'student_learning_activity_event',
+    teacherLogin: 'teacher_code_login',
+    teacherLogout: 'teacher_code_logout',
+    teacherDashboard: 'teacher_seminar_oop_dashboard_v1',
+    teacherDetail: 'teacher_seminar_oop_detail_v1'
+  }
+});
